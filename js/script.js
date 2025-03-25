787c8033af0176111174fc763af1a6d41891c2e3
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-03-25 11:22:07 +0000

    My User Input, with Javascript web page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kukwac
+// Created on: Sep 2022
 // This file contains the JS functions for index.html
+
+function enterClicked () {
+  // this function gets user's first name and age and displays it back to user
+
+  // get first name from text field
+    let firstName = document.getElementById("first-name").value
+	// get age from text field and cast it to integer
+    let Address = parseInt(document.getElementById("address").value)
+
+  // display name and age back to user
+    document.getElementById('user-info').innerHTML = "Your first name is " + firstName + " and you're address is " + Address + "."
+}
